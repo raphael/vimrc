3d0e456b72d5d55334df419eb5d79645c47c6266
Raphael Simon <simon.raphael@gmail.com>
2016-05-27 16:07:10 -0700

    Fix a few issues

---
+++ clean.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rm -rf autoload plugged
--- setup.sh
+++ setup.sh
@@ -6,19 +6,12 @@ today=`date +%Y%m%d`
 nvim="${XDG_CONFIG_HOME}/nvim"
 [ -e $nvim ] && [ ! -L $nvim ] && mv $nvim $nvim.$today
 
-echo "Creating symlinks"
-lnif() {
-  if [ ! -e $2 ] ; then
-    ln -s $1 $2
-  fi
-  if [ -L $2 ] ; then
-    ln -sf $1 $2
-  fi
-}
-lnif $PWD $nvim
+echo "Creating symlink"
+ln -s $PWD $nvim
 
-echo "Installing vim-plug"
+echo "Installing plugins"
 curl -fLo ${nvim}/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim -u . +PlugInstall +qall
+nvim -u init.vim +PlugInstall +qall
+nvim -u init.vim +UpdateRemotePlugins +qall
